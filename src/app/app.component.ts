9e89b9a5d1a7d5aa969616572bfd684879df0057
aashgar <aashgar@yahoo.com>
2021-12-20 23:04:55 +0200

    angapp5 submodule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './Student';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angapp5';
+  title = 'Angular App5';
+  names: string[] =['Mohammed', 'Ali', 'Sami'];
+  students: Student[]=[
+    {"id":111, "name": "Ahmad", "major": "SW", "grade": 89.99999},
+    {"id":222, "name": "Mostafa", "major": "CS", "grade": 79.99999},
+    {"id":333, "name": "Ahmad", "major": "Network", "grade": 69.99999}
+
+  ]
 }
